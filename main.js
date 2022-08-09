97af02f518207d0cfead0986701c65a486886e3d
Konstantin <76451056+Kostya-pixel@users.noreply.github.com>
2022-08-09 14:59:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+document.querySelector('.form__submit_btn').onclick = myClick;
+function myClick(){
+    let iName = document.querySelector('.input__name').value;
+    let tel = document.querySelector('.input__tel').value;
+    console.log(iName);
+    console.log(tel);
+}
+
